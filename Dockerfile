74140ad488f3c86c0e5d4b9182297af85208625b
Will Thames <wthames@skedulo.com>
2021-12-13 10:31:11 +1000

    Improved error handling and configuration

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.10.1-alpine3.15
 
 RUN pip install kubernetes-validate==1.22.1rc2
 
+USER 1000
+
 COPY entrypoint.sh /entrypoint.sh
 
-ENTRYPOINT /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,30 @@
-#!/bin/sh -le
+#!/bin/sh
+
+if [ $# -lt 3 ]; then
+  echo "expected: $0 <strict(true|false)> <version|latest> target"
+  echo "got: $@"
+  exit 1
+fi
 
 args=""
 if [[ "$1" != "false" ]] ; then
   args="${args} --strict"
 fi
+
 if [[ "$2" != "latest" ]] ; then
   args="${args} -k ${2}"
 fi
 
-output=$(kubernetes-validate ${args} $3)
+if [ -d "$3" ] ; then
+  output=$(find $3 -type f | xargs kubernetes-validate --quiet ${args})
+  rc=$?
+elif [ -f "$3" ] ; then
+  output=$(kubernetes-validate --quiet ${args} $3)
+  rc=$?
+else
+  echo "unexpected: $3 should be a file or a directory"
+  exit 2
+fi
 
 echo "::set-output name=output::$output"
+exit $rc
