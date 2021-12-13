8502f229c617ae7bb6c00f384f13f617a95c95e7
Will Thames <wthames@skedulo.com>
2021-12-13 14:53:52 +1000

    pipefail doesn't work with included ash
    We'll just split the pipe, it's not any less tidy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash -o pipefail
+#!/bin/ash
 
 if [ $# -lt 3 ]; then
   echo "expected: $0 <strict(true|false)> <version|latest> <quiet(true|false)> target"
@@ -20,15 +20,15 @@ if [[ "$3" != "false" ]] ; then
 fi
 
 if [ -d "$4" ] ; then
-  output=$(find $4 -type f | xargs kubernetes-validate ${args} | sed -z -e 's/%/%25/g' -e "s/\n/%0A/g" -e "s/\r/%0D/g")
+  output=$(find $4 -type f | xargs kubernetes-validate ${args})
   rc=$?
 elif [ -f "$4" ] ; then
-  output=$(kubernetes-validate ${args} $4 | sed -z -e 's/%/%25/g' -e "s/\n/%0A/g" -e "s/\r/%0D/g")
+  output=$(kubernetes-validate ${args} $4)
   rc=$?
 else
   echo "unexpected: $4 should be a file or a directory"
   exit 2
 fi
-
+output=$(echo $output | sed -z -e 's/%/%25/g' -e "s/\n/%0A/g" -e "s/\r/%0D/g")
 echo "::set-output name=output::${output}"
 exit $rc
